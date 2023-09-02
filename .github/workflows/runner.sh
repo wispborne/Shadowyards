54add0dd0dbdf6370bc286b6ddb0d0537d114fc2
Wisp <1343347+wispborne@users.noreply.github.com>
2023-09-01 18:14:47 -0700

    oops

--- .github/workflows/runner.sh
+++ .github/workflows/runner.sh
@@ -2,7 +2,7 @@
 
 ### Edit
 # Your mod name. The version number will be attached to this to form "My-Mod"
-MOD_FOLDER_NAME="Change-me-in-github-worksflows-runner-sh"
+MOD_FOLDER_NAME="Shadowyards (Wisp)"
 
 echo "Folder name will be $MOD_FOLDER_NAME"
 ###
